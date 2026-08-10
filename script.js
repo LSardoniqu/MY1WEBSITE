97528425c871a927d3bd8b7b19a9c3a97a7dd309
LSardoniqu <lucasaraujolourenco846@gmail.com>
2026-08-10 22:07:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const button = document.getElementById("helloButton");
+const pinInput = document.getElementById("pinInput");
 
 button.addEventListener("click", function () {
-  alert("Hello! You clicked the button.");
+  const pin = pinInput.value.trim();
+
+  if (/^\d{6}$/.test(pin)) {
+    alert("PIN accepted!");
+  } else {
+    alert("Please enter a 6-digit PIN.");
+  }
 });
